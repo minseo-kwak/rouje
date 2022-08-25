6a5c8c73c0f6e6c07b9493d281a49d31e6ffe599
Minseo <passxy@naver.com>
2022-08-25 18:49:27 +0900

    완성

--- js/main.js
+++ js/main.js
@@ -1,11 +1,43 @@
-// 모바일 메뉴
 $(function(){
+  //a tag 막기
+  $('a[href="#"]').click(function(ignore){
+    ignore.preventDefault();
+    alert('서브 페이지는 준비 중입니다.');
+  });
+
+  //모바일 메뉴
   $('.btn-show-menu').click(function(){
     $(this).toggleClass('btn-hide-menu');
     $('#mobile-menu').fadeToggle();
     $('header').toggleClass('open', 400);
   });
+
+  //탑 버튼
+  $("#btn-top").click(function() {   
+    $('html, body').animate({
+      scrollTop : 0
+     }, 500);
+     return false;
+     });
+
+  // 메뉴 스크롤 막기
+  $('#mobile-menu').on('scroll touchmove mousewheel', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+  });
+
+  // 탭메뉴
+  $('.tab > div').click(function() {
+    var activeTab = $(this).attr('data-tab');
+    $('.tab > div').removeClass('on');
+    $('.tabcont').removeClass('on');
+    $(this).addClass('on');
+    $('#' + activeTab).addClass('on');
+  });
 })
+
+
 
 //scroll animation
 window.onscroll = function(){scrollFunction()};
@@ -16,33 +48,9 @@ window.onscroll = function(){scrollFunction()};
                 document.getElementById('btn-top').style.bottom = '25px';
             } else {
                 document.getElementById('navbar').style.top = '-80px';
-                document.getElementById('btn-top').style.bottom = '-120px';
+                document.getElementById('btn-top').style.bottom = '-40px';
             }
     }
-
-$("#btn-top").click(function() {   
-    $('html, body').animate({
-      scrollTop : 0
-     }, 500);
-     return false;
-     });
-
-// 메뉴 스크롤 막기
-$('#mobile-menu').on('scroll touchmove mousewheel', function(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    return false;
-});
-
-// 탭메뉴
-$('.tab > div').click(function() {
-  var activeTab = $(this).attr('data-tab');
-  $('.tab > div').removeClass('on');
-  $('.tabcont').removeClass('on');
-  $(this).addClass('on');
-  $('#' + activeTab).addClass('on');
-  // $('#' + activeTab).fadeIn();
-});
 
 // new-arrival
 var slide = new Swiper(".newArrival", {
